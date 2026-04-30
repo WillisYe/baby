be6dba60ab9d54bcd0ddbb600d2ff4ae05d576a6
yewei <ywbyl@qq.com>
2026-04-30 16:35:02 +0800

    fix: 1.0.11

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,6 +1,6 @@
 export function getList() {
   return uni.request({
-    url: `https://ac016ac0-5001-4578-9218-8d475e137b06-00-2ojs7s7phnq3m.worf.replit.dev/api/babies`,
+    url: `https://baby-logbook--willisye.replit.app/api/babies`,
     method: 'GET',
     data: {}
   })
@@ -8,7 +8,7 @@ export function getList() {
 
 export function addItem(data) {
   return uni.request({
-    url: `https://ac016ac0-5001-4578-9218-8d475e137b06-00-2ojs7s7phnq3m.worf.replit.dev/api/babies`,
+    url: `https://baby-logbook--willisye.replit.app/api/babies`,
     method: 'POST',
     data: data,
   })
@@ -16,7 +16,7 @@ export function addItem(data) {
 
 export function editItem(data) {
   uni.request({
-    url: `https://ac016ac0-5001-4578-9218-8d475e137b06-00-2ojs7s7phnq3m.worf.replit.dev/api/babies/${data.deviceId}`,
+    url: `https://baby-logbook--willisye.replit.app/api/babies/${data.deviceId}`,
     method: 'PUT',
     data: data
   })
@@ -24,7 +24,7 @@ export function editItem(data) {
 
 export function getItem(deviceId) {
   return uni.request({
-    url: `https://ac016ac0-5001-4578-9218-8d475e137b06-00-2ojs7s7phnq3m.worf.replit.dev/api/babies/${deviceId}`,
+    url: `https://baby-logbook--willisye.replit.app/api/babies/${deviceId}`,
     method: 'GET',
     data: {}
   })
